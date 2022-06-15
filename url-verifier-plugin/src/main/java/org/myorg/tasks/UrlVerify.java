763fef8b2da28b8c4e1cc329e306a17d337dfcbc
kazurayam <kazuaki.urayama@gmail.com>
2022-06-15 11:44:33 +0900

    save progress

--- url-verifier-plugin/src/main/java/org/myorg/tasks/UrlVerify.java
+++ url-verifier-plugin/src/main/java/org/myorg/tasks/UrlVerify.java
@@ -3,7 +3,6 @@ package org.myorg.tasks;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 import org.myorg.http.DefaultHttpCaller;
 import org.myorg.http.HttpCallException;
@@ -18,14 +17,15 @@ abstract public class UrlVerify extends DefaultTask {
     public UrlVerify() {
         getUrl().convention("https://docs.gradle.org/current/userguide/testing_gradle_plugins.html");
     }
+
     @TaskAction
     public void action() throws HttpCallException {
         HttpCaller httpCaller = new DefaultHttpCaller();
         HttpResponse httpResponse = httpCaller.get(this.getUrl().get());
         if (httpResponse.getCode() == 200) {
-            System.out.println("Successfully resolved URL '${getUrl().get()}'");
+            System.out.println(String.format("Successfully resolved URL '%s'", getUrl().get()));
         } else {
-            System.err.println("Failed to resolve URL '${getUrl().get()}'");
+            System.err.println(String.format("Failed to resolve URL '%s'", getUrl().get()));
         }
     }
 }
